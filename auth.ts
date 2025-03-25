ffddd5cbe4772630aa1a486acd6498dbc0b50ce1
Marjorie Monson <20235962@esen.edu.sv>
2025-03-25 14:08:40 -0600

    cambio

--- auth.ts
+++
@@ -1,9 +0,0 @@
-import argon2 from "argon2";
-
-export async function hashPassword(plainPassword: string): Promise<string>{
-    return await argon2.hash(plainPassword);
-}
-
-export async function validatePassword(plainPassword: string, hashPassword: string): Promise<boolean>{
-    return await argon2.verify( hashPassword, plainPassword);
-};
